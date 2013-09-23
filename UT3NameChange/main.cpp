8955fc201e50a3ef11b6789f700b47986fea397b
Jan Niklas Hasse <jhasse@gmail.com>
2013-09-23 17:18:13 +0200

    First commit.

---
+++ UT3NameChange/main.cpp
@@ -0,0 +1,77 @@
+#include <windows.h>
+#include <iostream>
+#include <string>
+#include <shlobj.h>
+#include <vector>
+#include <sstream>
+#include <fstream>
+
+#pragma comment(lib, "shell32.lib")
+
+class PauseAtExit {
+public:
+	~PauseAtExit() {
+		system("pause");
+	}
+};
+
+int main() {
+	PauseAtExit _;
+	std::cout << "Enter your Unreal Tournament 3 multiplayer name: " << std::flush;
+	std::string name;
+	std::cin >> name;
+
+	std::vector<char> my_documents(MAX_PATH);
+	HRESULT result = SHGetFolderPath(NULL, CSIDL_PERSONAL, NULL, SHGFP_TYPE_CURRENT, &my_documents[0]);
+	if (result != S_OK) {
+		std::cout << "Error: " << result << "\n";
+		return EXIT_FAILURE;
+	}
+
+	std::stringstream sstream;
+	sstream << &my_documents[0] << "\\My Games\\Unreal Tournament 3\\UTGame\\Config\\UTGame.ini";
+	auto iniName = sstream.str();
+	std::ifstream fin(iniName);
+	if (!fin) {
+		std::cerr << "File not found: " << sstream.str() << std::endl;
+		return EXIT_FAILURE;
+	}
+	sstream << ".bak";
+	std::ofstream fout(sstream.str());
+
+	bool success = false;
+	while (fin) {
+		std::string line;
+		std::getline(fin, line);
+		fout << line << std::endl;
+	}
+
+	fin.close();
+	fout.close();
+	fin.open(sstream.str());
+	fout.open(iniName);
+	while (fin) {
+		std::string line;
+		std::getline(fin, line);
+		if (line == "PlayerNames=") {
+			success = true;
+			fout << "PlayerNames=" << name << std::endl;
+		}
+		fout << line << std::endl;
+	}
+
+	if (!success) {
+		std::cout << "Error: Couldn't set player name in UTGame.ini." << std::endl;
+		return EXIT_FAILURE;
+	}
+
+	std::stringstream cmd;
+	cmd << "copy \""
+		<< &my_documents[0] << "\\My Games\\Unreal Tournament 3\\UTGame\\SaveData\\.ue3profile\" \""
+	    << &my_documents[0] << "\\My Games\\Unreal Tournament 3\\UTGame\\SaveData\\" << name << ".ue3profile\"";
+	std::cout << std::endl << cmd.str() << std::endl << std::endl;
+	if (system(cmd.str().c_str())) {
+		std::cout << "Error: Copying profile file failed." << std::endl;
+	}
+	std::cout << "\nSuccess!" << std::endl;
+}
